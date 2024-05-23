86a1ed5c901981dfe6b9526574b9ba34a59668b5
Guillaume-tllrd <guillaumetillard10@gmail.com>
2024-05-23 11:14:25 +0200

    initial commit

--- php/08a_user_add.php
+++ php/08a_user_add.php
@@ -10,18 +10,28 @@ $gender = "male";
 $birth_date = "1958-12-28";
 $country = "Brazil";
 
-$sql = "INSERT INTO users (first_name, last_name, email, gender, birth_date, country) VALUES (':first_name', ':last_name', ':email', ':gender', ':birth_date', ':country') ;
+
+$sql = "INSERT INTO users (first_name, last_name, email, gender, birth_date, country ) VALUES (:first_name, :last_name, :email, :gender, :birth_date, :country)" ;
 
 
 // Préparation de la requête
 $query = $db->prepare($sql);
--- $query->bindValue(':first_name', $first_name);
--- $query->bindValue(':last_name', $last_name);
+$query->bindValue(':first_name', $first_name);
+$query->bindValue(':last_name', $last_name);
+$query->bindValue(':email', $email);
+$query->bindValue(':gender', $gender);
+$query->bindValue(':birth_date', $birth_date);
+$query->bindValue(':country', $country);
 // Exécution de la requête
 $query->execute();
+if ($query->execute()) {
+    echo "Nouvel utilisateur ajouté avec succès";
+} else {
+    echo "Erreur lors de l'ajout de l'utilisateur";
+}
 
 require_once("./src/close.php");
 
-// Redirection vers la page d'accueil
+//Redirection vers la page d'accueil
 header('Location: ./index.php');
 ?>
